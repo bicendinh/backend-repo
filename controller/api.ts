dd9028c010a08c44612b95d289a7236bf862b1aa
Quang Khang <bicendinh.dev@gmail.com>
2024-08-07 21:26:39 +0700

    backend-repo:delete user api

--- controller/api.ts
+++ controller/api.ts
@@ -1,13 +1,59 @@
-import { Request, Response } from 'express';
-import { firestore } from '@config/firebaseConfig';
+import { Request, Response } from "express";
+import { firestore } from "@config/firebaseConfig";
 
-// Example controller function
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const usersSnapshot = await firestore.collection('users').get();
-    const users = usersSnapshot.docs.map(doc => doc.data());
+    const usersSnapshot = await firestore.collection("users").get();
+    const users = usersSnapshot.docs.map((doc) => doc.data());
     res.json(users);
   } catch (error) {
-    res.status(500).json({ message: 'Error retrieving users', error });
+    res.status(500).json({ message: "Error retrieving users", error });
+  }
+};
+
+export const upsertUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const userId = userData?.id;
+
+    if (!userData) {
+      return res
+        .status(400)
+        .json({ message: "User ID and user data are required" });
+    }
+
+    let userRef;
+
+    if (userId) {
+      userRef = firestore.collection("users").doc(userId);
+    } else {
+      userRef = firestore.collection("users").doc();
+      userData.id = userRef.id;
+    }
+
+    // Upsert the user document
+    await userRef.set(userData, { merge: true });
+
+    res.status(200).json({ message: "User upserted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error upserting user", error });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const userDoc = firestore.collection("users").doc(id);
+    const userSnapshot = await userDoc.get();
+
+    if (!userSnapshot.exists) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await userDoc.delete();
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting user", error });
   }
 };
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
-import { getUsers } from '@controller/api';
-import { authMiddleware } from '@middleware/authMiddleware';
+import { Router } from "express";
+import { getUsers, upsertUser, deleteUser } from "@controller/api";
+import { authMiddleware } from "@middleware/authMiddleware";
 
 const router = Router();
 
 // Routes with authentication middleware
-router.get('/', authMiddleware, getUsers);
+router.get("/fetch-user-data", authMiddleware, getUsers);
+router.post("/update-user-data", authMiddleware, upsertUser);
+router.delete("/delete-user-data/:id", authMiddleware, deleteUser);
 
 export default router;
